6531a904540dc7d57e9b99329cf6ddae95242d69
Emmanuel Arenas Tolteca <1darenastolteca@gmail.com>
2021-03-09 18:00:39 -0600

    Creando archivo para la primera clase de audio en web

---
+++ scripts/audioGraf.js
@@ -0,0 +1,161 @@
+let osc1, fft;
+let sliderFreq;
+let sliderVol;
+let isLog;
+let textColor;
+
+function setup()
+{
+  createCanvas(800, 600);
+  fill(255);
+  colorMode(HSB);
+  sliderFreq = createSlider(0,800,0,0.1);
+  sliderFreq.style('width: 800px');
+  sliderVol = createSlider(0,1,0,0.01);
+  sliderVol.style('transform: rotate(-90deg);');
+  osc1 = new p5.Oscillator();
+  osc1.setType('sine');
+  isLog = true;
+
+  
+  osc1.freq(sliderFreq.value())
+  osc1.amp(sliderVol.value());
+  osc1.start();
+  
+  fft = new p5.FFT();
+  textColor = color(160,255,80);
+}
+
+function draw()
+{
+  background(0);
+  
+  // Sliders
+  let freq = logslider(sliderFreq.value(), 0,800,20,20000);
+  let vol = sliderVol.value();
+  osc1.freq(freq);
+  osc1.amp(vol);
+
+  // Texto de informacion
+  fill(textColor);
+  text('Oscilador: ' + osc1.getType(),width/5,50);
+  text(freq + '  Hz',width*2/5, 50);
+  text(vol + ' vol', width*3/5, 50);  
+  
+  // Espectro de frecuencias
+  let spectrum = fft.analyze(1024);
+  noStroke();
+  for (let i = 0; i< spectrum.length; i++){
+    let c = map(i,0, spectrum.length, 0,255)
+    fill(c,255,255); //remove stroke(255);
+
+    if(isLog) {
+      let a = map(log(i), 0, log(spectrum.length), 0, width) 
+      let b = map(spectrum[i], 0, 255, 0, height/3)
+      rect(a, height, width / spectrum.length, -b)
+      fill(textColor);
+      text('LOG',width*3/4,50);
+
+    } else {
+      let a = map(i, 0, spectrum.length, 0, width);
+      let b = map(spectrum[i], 0, 255, 0,height/3);
+      rect(a, height, width / spectrum.length, -b )
+      fill(textColor);
+      text('LIN',width*3/4,50);
+    } 
+    
+    
+    
+  }
+  
+  // Forma de onda en el tiempo
+  let waveform = fft.waveform();
+  noFill();
+  beginShape();
+  stroke(100,255,255);
+  for (let i = 0; i < waveform.length; i++){
+    let x = map(i, 0, waveform.length, 0, width);
+    let y = map( waveform[i], -1, 1, 50, 50+height*3/5);
+    vertex(x,y);
+  }
+  endShape();
+
+  
+}
+
+function keyPressed() {
+  if(key == '1')
+    osc1.setType('sine');
+  if(key == '2')
+    osc1.setType('triangle');
+  if(key == '3')
+    osc1.setType('square');
+  if(key == '4')
+  osc1.setType('sawtooth');
+  if(key == 'p')
+  osc1.start();
+  if(key == 's')
+  osc1.stop();
+  if(key == 'l')
+    isLog =! isLog;
+    print(isLog);
+}
+
+function logslider(position, min ,max, minLog, maxLog) {
+  var minp = min;
+  var maxp = max;
+  var minv = Math.log(minLog);
+  var maxv = Math.log(maxLog);
+  var scale = (maxv-minv) / (maxp-minp);
+
+  return Math.exp(minv + scale*(position-minp));
+}
+
+
+function autoCorrelate(buffer) {
+  var newBuffer = [];
+  var nSamples = buffer.length;
+
+  var autocorrelation = [];
+
+  // center clip removes any samples under 0.1
+  if (centerClip) {
+    var cutoff = 0.1;
+    for (var i = 0; i < buffer.length; i++) {
+      var val = buffer[i];
+      buffer[i] = Math.abs(val) > cutoff ? val : 0;
+    }
+  }
+
+  for (var lag = 0; lag < nSamples; lag++){
+    var sum = 0; 
+    for (var index = 0; index < nSamples; index++){
+      var indexLagged = index+lag;
+      if (indexLagged < nSamples){
+        var sound1 = buffer[index];
+        var sound2 = buffer[indexLagged];
+        var product = sound1 * sound2;
+        sum += product;
+      }
+    }
+
+    // average to a value between -1 and 1
+    newBuffer[lag] = sum/nSamples;
+  }
+
+  if (bNormalize){
+    var biggestVal = 0;
+    for (var index = 0; index < nSamples; index++){
+      if (abs(newBuffer[index]) > biggestVal){
+        biggestVal = abs(newBuffer[index]);
+      }
+    }
+    for (var index = 0; index < nSamples; index++){
+      newBuffer[index] /= biggestVal;
+    }
+  }
+
+  return newBuffer;
+}
+
+  
